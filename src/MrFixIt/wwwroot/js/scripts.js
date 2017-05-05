f0ffce18e41adb99edbe2239bdef0d13b3bc60a4
Cassie Musolf <cassiemusolf@gmail.com>
2017-05-05 11:19:53 -0700

    begin writing ajax call to have a worker claim a job.

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,14 @@
-﻿var test = "it works";
+﻿//$(document).ready(function () {
+//    $(".click-claim").click(function () {
+//        $.ajax({
+//            type: 'POST',
+//            dataType: 'html',
+//            url: 'Jobs/Claim/' + this.JobId,
+//            success: function (result) {
+//                console.log(this.JobId);
+//                var resultMessage = 'It worked!';
+//                $('#results').html(resultMessage);
+//            }
+//        });
+//    });
+//});
